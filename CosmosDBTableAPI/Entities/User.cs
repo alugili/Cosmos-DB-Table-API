b9cf359939fecd5464d0f4db396573bbdbbc68f7
Bassam Alugili <balugili@hotmail.com>
2019-06-17 07:19:38 +0000

    Add files via upload

--- CosmosDBTableAPI/Entities/User.cs
+++ CosmosDBTableAPI/Entities/User.cs
@@ -1,1 +1,22 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Table;
 
+namespace CosmosDBTableAPI
+{
+  public class User : TableEntity
+  {
+    public string EMail { get; set; }
+
+    public DateTimeOffset LastLogin { get; set; }
+
+    public User()
+    {
+    }
+
+    public User(string locationId, string type)
+    {
+      PartitionKey = locationId;
+      RowKey = type;
+    }
+  }
+}
